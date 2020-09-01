2b423b81403baf11c908009a16cdfc92fac22ff1
ArtyomRattik <44434816+ArtyomRattik@users.noreply.github.com>
2020-09-01 17:48:00 +0300

    Update nav.php

--- newsite/nav.php
+++ newsite/nav.php
@@ -1,22 +1,51 @@
-<nav class="navbar navbar-inverse navbar-fixd-top">
-		<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
-				aria-expanded="false" aria-controls="navbar">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="index.php">Project name</a>
-			</div>
-			<div id="navbar" class="collapse navbar-collapse">
-				<ul class="nav navbar-nav">
+<html>
+<head>
+<style>
+ul {
+	list-style-type: none;
+	margin: 0;
+	padding: 0;
+	overflow: hidden;
+	background-color: grey;
+}
+li {
+	float: left;
+}
+li a {
+	display: block;
+	color: white;
+	text-align: center;
+	padding: 15px;
+	text-decoration: none;
+}
+li a: hover {
+	background-color: black;
+}
+.column-right{ float: right; width: 50%; text-align: left}
+.column-left{ float: left; width: 50%; text-align: center}
+</style>
+</head>
+<body>
+
+<nav class="navbar">
+	<div class="container">
+		<div id="navbar" class="collapse navbar-collapse">
+			<ul class="nav navbar-nav">
+				<div class="column-left">
+					<li><a class="navbar-brand" href="index.php">Project name</a></li>
 					<li class="active"><a href="index.php">Home</a></li>
 					<li><a href="profile.php">Profile</a></li>
 					<li><a href="mytrades.php">My Trades</a></li>
 					<li><a href="inventory.php">Inventory</a></li>
-				</ul>
-			</div>
+				</div>
+				<div class="column-right">
+					<li><a href="profile.php">Profile</a></li>
+				</div>
+
+			</ul>
 		</div>
-	</nav>
+	</div>
+</nav>
+
+</body>
+</html>
